b3e49165b1b2a25bd22831c8a486a19f83914354
Gaboa <dmitry.zhabko@gmail.com>
2016-09-21 12:38:58 +0300

    test module

---
+++ markup/components/test/test.js
@@ -0,0 +1,3 @@
+export function test(name) {
+    console.log(`I am ${name}!`);
+}
--- markup/static/js/main.js
+++ markup/static/js/main.js
@@ -1,5 +1,5 @@
 'use strict';
 
-/*
-    This file can be used as entry point for webpack!
- */
+import { test } from 'components/test/test';
+
+test('Dima');
