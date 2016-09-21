6427ab7c9db8d77545c93a5eaed3401d104b6d8c
Nastya <nastya.volkovinskaya@gmail.com>
2016-09-21 18:26:55 +0300

    new conf commit

--- markup/components/cool/cool.js
+++ markup/components/cool/cool.js
@@ -1,4 +1,4 @@
 function reallyCool(name) {
     console.log(`You are cool cool: ${name}`);
-    console.log(`You are really cool: ${name} and I am too!`);
+    console.log(`You are really cool and will become better: ${name} and I am too!`);
 }
